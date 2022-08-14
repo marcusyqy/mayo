8a5e2c8f7d3ad3c676e97989b910b7af7371823f
Marcus Yong <marcusyqy@gmail.com>
2022-08-14 15:23:28 +0800

    Formatted and moved Log from `main` to `core`

--- src/main/EntryPoint.cpp
+++ src/main/EntryPoint.cpp
@@ -1,26 +1,23 @@
 #include "EntryPoint.hpp"
-#include "Log.hpp"
+#include "core/Log.hpp"
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 
 namespace zoo {
 
-static void errorCallback(int error, const char* description)
-{
+static void errorCallback(int error, const char* description) {
     ZOO_LOG_ERROR("Error: {}", description);
 }
 
-static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void keyCallback(
+    GLFWwindow* window, int key, int scancode, int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
 Application::ExitStatus Main(Application::Settings args) noexcept {
-
     ZOO_LOG_INFO("Starting application");
-
 
     glfwSetErrorCallback(errorCallback);
 
@@ -30,9 +27,10 @@ Application::ExitStatus Main(Application::Settings args) noexcept {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+    GLFWwindow* window =
+        glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+
+    if (!window) {
         glfwTerminate();
         return Application::ExitStatus::Err;
     }
@@ -42,8 +40,7 @@ Application::ExitStatus Main(Application::Settings args) noexcept {
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         // glfwGetFramebufferSize(window, &width, &height);
         glfwSwapBuffers(window);
         glfwPollEvents();
