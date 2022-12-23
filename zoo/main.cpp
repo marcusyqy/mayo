edee4ada95a7808a68f282fce1c625f887dd3e1d
Marcus  Yong <marcusyqy@gmail.com>
2022-12-24 01:50:08 +0800

    Update main.cpp
    Changed from trailing return to normal

--- zoo/main.cpp
+++ zoo/main.cpp
@@ -1,7 +1,7 @@
 
 #include "main/entry_point.hpp"
 
-auto main(int argc, char* argv[]) -> int { // NOLINT
+int main(int argc, char* argv[]) { // NOLINT
     zoo::application::exit_status status =
         zoo::main(zoo::application::settings{argc, argv});
     return status == zoo::application::exit_status::ok ? 0 : -1;
