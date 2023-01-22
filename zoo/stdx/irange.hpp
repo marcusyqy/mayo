1102953dd459994a363a85b1941c12e3ab1c94ab
Marcus Yong <marcusyqy@gmail.com>
2023-01-22 23:49:09 +0800

    fix problem with `irange`

--- zoo/stdx/irange.hpp
+++ zoo/stdx/irange.hpp
@@ -1,11 +1,13 @@
 
 #pragma once
+#include "stdx/function_ref.hpp"
+#include "type_traits.hpp"
 #include <type_traits>
 
 namespace stdx {
 
-template<typename I, std::enable_if<std::is_integral_v<I>, bool> = true>
-class irange {
+template<typename I, std::enable_if_t<std::is_integral_v<I>, bool> = true>
+class integer_range {
 public:
     struct iterator {
         iterator(I value) noexcept : value_(value) {}
@@ -28,6 +30,25 @@ public:
             return value_ == e.value_;
         }
 
+        bool operator!=(const iterator& e) const noexcept {
+            return value_ != e.value_;
+        }
+
+        bool operator<(const iterator& e) const noexcept {
+            return value_ < e.value_;
+        }
+
+        bool operator>(const iterator& e) const noexcept {
+            return value_ > e.value_;
+        }
+        bool operator<=(const iterator& e) const noexcept {
+            return value_ <= e.value_;
+        }
+
+        bool operator>=(const iterator& e) const noexcept {
+            return value_ >= e.value_;
+        }
+
     private:
         I value_;
     };
@@ -36,12 +57,22 @@ public:
     const_iterator begin() const noexcept { return begin_; }
     const_iterator end() const noexcept { return end_; }
 
+    void for_each(stdx::function_ref<void(I)> f) noexcept {
+        for (auto i : *this) {
+            f(i);
+        }
+    }
+
+    integer_range(I begin, I end) : begin_(begin), end_(end) {}
+
 private:
     I begin_;
     I end_;
 };
 
 template<typename I>
-irange(I, I) -> irange<I>;
+integer_range<I> irange(stdx::type_identity_t<I> begin, I end) noexcept {
+    return {begin, end};
+}
 
 } // namespace stdx
