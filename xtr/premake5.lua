4d9b6c3a81f9811dec288c853d6f29a01fd8b43c
Marcus Yong <marcusyqy@gmail.com>
2023-02-17 02:32:33 +0800

    added vma because i'm lazy

--- xtr/premake5.lua
+++ xtr/premake5.lua
@@ -1,10 +1,46 @@
-project "xtr"
+group "xtr"
+
+project "refl"
     language "C++"
     cppdialect "C++17"
     staticruntime "on"
     characterset "MBCS"
     kind "None"
     files {
-        "**.hpp",
-        "**.h"
+        "%{prj.name}/**.hpp",
+        "%{prj.name}/**.cpp",
+        "%{prj.name}/**.h"
+    }
+
+project "stdx"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    characterset "MBCS"
+    kind "None"
+    files {
+        "%{prj.name}/**.hpp",
+        "%{prj.name}/**.cpp",
+        "%{prj.name}/**.h"
+    }
+
+project "vma"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    characterset "MBCS"
+    targetdir("bin/" .. outputdir .. "/%{prj.name}")
+    objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+    kind "StaticLib"
+    warnings "Extra"
+    includedirs {
+        "%{include_dir.vulkan}"
+    }
+    links {
+        "%{library_dir.vulkan}"
+    }
+    files {
+        "%{prj.name}/**.hpp",
+        "%{prj.name}/**.cpp",
+        "%{prj.name}/**.h"
     }
--- zoo/premake5.lua
+++ zoo/premake5.lua
@@ -24,6 +24,7 @@ project "zoo"
 
     links {
         "GLFW",
+        "vma",
         "%{library_dir.vulkan}"
     }
 
