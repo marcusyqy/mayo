5033c0bf8f03b1cabc796904fbd4e977ac510739
Marcus Yong <marcusyqy@gmail.com>
2023-07-03 23:37:50 +0800

    some more comments

--- zoo/render/resources/Mesh.cpp
+++ zoo/render/resources/Mesh.cpp
@@ -94,7 +94,8 @@ MeshData load_mesh_data(std::string_view file_name) {
                 attrib.normals[3 * index.normal_index + 1],
                 attrib.normals[3 * index.normal_index + 2] };
 
-            vertex.color = { 1.0, 1.0, 1.0 };
+            // TODO: change this part to white again
+            vertex.color = vertex.normal; //  { 1.0, 1.0, 1.0 };
             vertex.uv = { attrib.texcoords[2 * index.texcoord_index + 0],
                 1.0f - attrib.texcoords[2 * index.texcoord_index + 1] };
 
