e26b978b2cd6763d2480f12810aa1c37b981d56f
Marcus Yong <marcusyqy@gmail.com>
2023-09-13 00:08:40 -0400

    some initial things

--- build.cpp
+++ build.cpp
@@ -1,10 +1,15 @@
-#include "ccb/ccb.hpp"
 #include "zoo/build.hpp"
+#include "ccb/ccb.hpp"
 #include <iostream>
 
 int main(int argc, char** argv) {
+    using namespace ccb;
+
+    Workspace ws;
+    ws.name = "zoo";
+    ws.architecture = Architecture::x86_64;
+
     static_cast<void>(argc);
     static_cast<void>(argv);
     std::cout << "hello" << std::endl;
 }
-
--- ccb/ccb.hpp
+++ ccb/ccb.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <string>
+
 namespace ccb {
 
-}
+enum class Architecture {
+    UNIVERSAL, // The universal binaries supported by iOS and macOS
+    x86,
+    x86_64,
+    ARM,
+    ARM64,
+};
 
+enum class Platform { WINDOWS, LINUX, MAC };
+enum class Compiler { CLANG, MICROSOFT, GCC };
 
+#ifdef WIN32
+struct Query {
+    constexpr static Platform platform         = Platform::WINDOWS;
+    constexpr static Compiler default_compiler = Compiler::MICROSOFT;
+};
+#else
+static_assert(false, "OTHER PLATFORMS OTHER THAN WINDOWS NOT SUPPORTED YET");
+#endif
 
+template <Compiler compiler = Query::default_compiler>
+struct Workspace {
+    std::string name;
+    Architecture architecture;
+};
+
+} // namespace ccb
